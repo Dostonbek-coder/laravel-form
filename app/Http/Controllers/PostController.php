3e6911384cb51ad2730c80930dc637f707105b4f
Dostonbek-coder <mahmudovdostonbek455@gmail.com>
2025-03-11 16:22:23 +0500

    Bugging errors in PostController file

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,15 +1,33 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
-   {
-  return 'Barcha postlar';
-   }
-}
+    {
+        return view('index');
+    }
 
+    public function store(Request $request)
+    {
+        return $request->all();
+    }
+
+
+    public function delete(Request $request)
+    {
+        return $request->all();
+    }
+
+    public function update(Request $request)
+    {
+        return $request->all();
+    }
+
+    public function patch(Request $request)
+    {
+        return $request->all();
+    }
+}
