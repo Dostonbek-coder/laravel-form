e431bfeefb43e70d23e96dcd5247d75d9b6f2666
Dostonbek-coder <mahmudovdostonbek455@gmail.com>
2025-03-11 16:31:44 +0500

    All methods in the web.php file

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,10 @@ use App\Http\Controllers\PostController;
 
 
 Route::get('/form', [PostController::class, 'index']);
-
 Route::post('/form', [PostController::class, 'store']);
 Route::delete('/form', [PostController::class, 'delete']);
 Route::put('/form', [PostController::class, 'update']);
 Route::patch('/form', [PostController::class, 'patch']);
+
+
 
