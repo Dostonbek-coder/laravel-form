7294c12491af947005bee7c54383ae7956baf296
Dostonbek-coder <mahmudovdostonbek455@gmail.com>
2025-03-12 15:02:08 +0500

    [Route name] errors have been corrected

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,12 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\PostController;
 
 
-Route::get('/index', [PostController::class, 'index']);
-Route::post('/store', [PostController::class, 'store']);
-Route::delete('/delete', [PostController::class, 'delete']);
-Route::put('/update', [PostController::class, 'update']);
-Route::patch('/patch', [PostController::class, 'patch']);
+
+Route::get('/index', [PostController::class, 'index'])->name('index');
+Route::post('/store', [PostController::class, 'store'])->name('store');
+Route::delete('/delete', [PostController::class, 'delete'])->name('delete');
+Route::put('/update', [PostController::class, 'update'])->name('update');
+Route::patch('/patch', [PostController::class, 'patch'])->name('patch');
 
 
 
